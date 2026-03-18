d956c36c623a0d52b16b334615a3b61e97521419
Aigeninc <vavier@gmail.com>
2026-03-17 22:57:22 -0500

    fix: bump cache version for practice planner deploy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for offline support
 // Update CACHE_VERSION when assets change to bust old caches
-const CACHE_VERSION = '20260316-rollback-clean';
+const CACHE_VERSION = '20260317-practice-planner';
 const CACHE_NAME = 'playbook-' + CACHE_VERSION;
 
 const ASSETS = [
